cd4099b8bf00db87c351d8b1f5d4aa5495b8badd
Japa <michalskikonrad@frozenway.games>
2026-08-02 14:14:17 +0200

    Add stamina pickup

---
+++ Source/P2C/Gameplay/Arena/Pickups/P2CStaminaPickup.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+
+#include "P2CCharacter.h"
+#include "Components/SphereComponent.h"
+#include "P2CStaminaPickup.generated.h"
+
+UCLASS(Abstract)
+class P2C_API AP2CStaminaPickup : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AP2CStaminaPickup();
+
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(
+		VisibleAnywhere,
+		BlueprintReadOnly,
+		Category = "P2C|Pickup"
+	)
+	TObjectPtr<USphereComponent> CollisionComponent;
+	
+	UPROPERTY(
+		VisibleAnywhere,
+		BlueprintReadOnly,
+		Category = "P2C|Pickup"
+	)
+	TObjectPtr<UStaticMeshComponent> PickupMesh;
+
+	UPROPERTY(
+		EditDefaultsOnly,
+		BlueprintReadOnly,
+		Category = "P2C|Pickup",
+		meta = (ClampMin = "1.0")
+	)
+	float StaminaRestoreAmount = 20.0f;
+	
+private:
+	UFUNCTION()
+	void HandleOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComponent,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+
+	bool TryCollect(AP2CCharacter* Character);
+};
